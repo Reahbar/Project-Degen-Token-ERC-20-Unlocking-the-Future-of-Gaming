882b5e93447fde0db140b5ca3a07838151df4b87
Reahbar <132294046+Reahbar@users.noreply.github.com>
2023-07-13 16:38:50 +0530

    Update Project.sol

--- Project.sol
+++ Project.sol
@@ -6,9 +6,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, Ownable {
 
-    constructor() ERC20("Degen", "DGN") {}
+    string[] public ShowItems;
+
+    constructor() ERC20("Degen", "DGN") {
+            ShowItems.push("Offical INR NFT");
+            ShowItems.push("Offical INR Gen2 NFT");
+            ShowItems.push("Random Reward!"); 
+    }
 
         function mint(address to, uint256 amount) public onlyOwner {
             _mint(to, amount);
+          
+    }
+
+       function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    
+    function purchase(uint256 productIndex) public {
+        require(productIndex >= 0 && productIndex < ShowItems.length, "Invalid Product Index!");
+        uint256 price = calculatePrice(productIndex);
+
+        transferFrom(msg.sender, owner(), price);
+    }
+
+    function calculatePrice(uint256 productIndex) private pure returns (uint256) {
+        uint256[] memory prices = new uint256[](3);
+        prices[0] = 100;  
+        prices[1] = 200;  
+        prices[2] = 300;   
+
+        return prices[productIndex];
     }
 }
